cbabe14baf428af183513f95d1d09cb1b3fd9b2c
shamimulhaque162 <khandoker15-1992@diu.edu.bd>
2022-12-18 01:22:09 +0600

    deployed api added

--- src/helpers/basrURL.js
+++ src/helpers/basrURL.js
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:3003/api'
-export const baseUrl = 'http://localhost:5008/api'
+export const baseUrl = 'https://redux-acc-assingment-1-server-production.up.railway.app/api'
